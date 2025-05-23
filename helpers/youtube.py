2ef5b396b5a6fb707ac50de930055627dcbc2dd7
Brayan503D <112528229+Brayan503D@users.noreply.github.com>
2025-05-23 07:21:46 -0600

    Update youtube.py

--- helpers/youtube.py
+++ helpers/youtube.py
@@ -108,5 +108,8 @@ def download_youtube():
         return {"error": "Falta el parámetro url"}
     return descargar_archivo_youtube(url)
 
+print(f"App escuchando en el puerto {port}")
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
