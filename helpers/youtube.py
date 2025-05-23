d33b78a87c2e545a951c8033cfe9b024000c4dc7
Brayan503D <112528229+Brayan503D@users.noreply.github.com>
2025-05-22 22:24:25 -0600

    Update youtube.py

--- helpers/youtube.py
+++ helpers/youtube.py
@@ -1,6 +1,5 @@
 from yt_dlp import YoutubeDL
-import requests
-from flask import Flask, send_file, Response, request
+from flask import Flask, send_file, request
 import os
 
 app = Flask(__name__)
@@ -41,7 +40,6 @@ def obtener_info_youtube(url):
                 'thumbnail': info.get('thumbnail'),
                 'url': video_url,
                 'formatos_disponibles': [f"{f['resolucion']}p (itag: {f['itag']})" for f in formatos_disponibles],
-                'reproducir_api': f"{request.host_url}stream/youtube?url={url}",
                 **apis_descarga
             }
     except Exception as e:
@@ -66,37 +64,6 @@ def descargar_archivo_youtube(url):
     except Exception as e:
         return {'error': str(e)}
 
-def reproducir_stream_youtube(url):
-    try:
-        ydl_opts = {
-            "format": "18",
-            "quiet": True,
-            "skip_download": True,
-            "noplaylist": True,
-            "cookiefile": "cookies.txt"
-        }
-        with YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=False)
-            video_url = info.get("url")
-
-        headers = {}
-        if 'Range' in request.headers:
-            headers['Range'] = request.headers['Range']
-
-        r = requests.get(video_url, headers=headers, stream=True)
-
-        return Response(
-            r.iter_content(chunk_size=1024),
-            status=r.status_code,
-            content_type=r.headers.get("Content-Type", "video/mp4"),
-            headers={
-                key: value for key, value in r.headers.items()
-                if key.lower() in ["content-length", "content-range", "accept-ranges", "content-type"]
-            }
-        )
-    except Exception as e:
-        return {'error': str(e)}
-
 @app.route("/info/youtube")
 def info_youtube():
     url = request.args.get("url")
@@ -110,13 +77,6 @@ def download_youtube():
     if not url:
         return {"error": "Falta el parámetro url"}
     return descargar_archivo_youtube(url)
-
-@app.route("/stream/youtube")
-def stream_youtube():
-    url = request.args.get("url")
-    if not url:
-        return {"error": "Falta el parámetro url"}
-    return reproducir_stream_youtube(url)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
