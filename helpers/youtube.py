a82bd0909683dd96252143c0e6a5f7f5d3e440a2
Brayan503D <112528229+Brayan503D@users.noreply.github.com>
2025-05-22 18:03:19 -0600

    Update youtube.py

--- helpers/youtube.py
+++ helpers/youtube.py
@@ -2,9 +2,19 @@ from yt_dlp import YoutubeDL
 
 def descargar_youtube(url):
     try:
-        ydl_opts = {'quiet': True, 'skip_download': True, 'format': 'best', 'noplaylist': True}
+        ydl_opts = {
+            'quiet': True,
+            'skip_download': True,
+            'format': 'best',
+            'noplaylist': True,
+            'cookiefile': 'cookies'  # archivo de cookies llamado "cookies"
+        }
         with YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=False)
-            return {'title': info.get('title'), 'url': info.get('url'), 'thumbnail': info.get('thumbnail')}
+            return {
+                'title': info.get('title'),
+                'url': info.get('url'),
+                'thumbnail': info.get('thumbnail')
+            }
     except Exception as e:
         return {'error': str(e)}
