b4cd05acbe1dd88807967547b94b62502cd5e6fa
Brayan503D <112528229+Brayan503D@users.noreply.github.com>
2025-05-23 07:28:36 -0600

    Update youtube.py

--- helpers/youtube.py
+++ helpers/youtube.py
@@ -114,5 +114,4 @@ def download_youtube():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    print(f"App escuchando en el puerto {port}")
     app.run(host="0.0.0.0", port=port)
