a0d5df816e521c75683b761c308b2dc832315cda
Brayan503D <112528229+Brayan503D@users.noreply.github.com>
2025-05-23 10:08:16 -0600

    Update youtube.py

--- helpers/youtube.py
+++ helpers/youtube.py
@@ -5,47 +5,6 @@ from yt_dlp import YoutubeDL
 import os
 import traceback
 
-def obtener_info_youtube(url, host_url=None):
-    try:
-        opciones = {
-            "quiet": True,
-            "skip_download": True,
-            "noplaylist": True,
-            "cookiefile": "cookies.txt",
-        }
-
-        with YoutubeDL(opciones) as ydl:
-            info = ydl.extract_info(url, download=False)
-
-        if not host_url:
-            host_url = "https://api-downloader-2cuz.onrender.com/"
-        elif not host_url.endswith("/"):
-            host_url += "/"
-
-        formatos = []
-        for f in info.get("formats", []):
-            if f.get("vcodec") != "none" and f.get("ext") == "mp4" and f.get("height"):
-                size = f.get("filesize") or 0
-                formatos.append({
-                    "itag": f["format_id"],
-                    "resolucion": f["height"],
-                    "filesize_mb": round(size / 1024 / 1024, 2),
-                    "descargar_url": f"{host_url}download/youtube/file?url={url}&itag={f['format_id']}"
-                })
-
-        formatos.sort(key=lambda x: x["resolucion"])
-
-        return {
-            "title": info.get("title"),
-            "thumbnail": info.get("thumbnail"),
-            "formatos_disponibles": formatos
-        }
-
-    except Exception:
-        error_trace = traceback.format_exc()
-        print(error_trace)
-        return {"error": "No se pudo obtener información del video.", "trace": error_trace}
-
 def descargar_archivo_youtube(url, itag):
     try:
         # Obtener información
@@ -81,12 +40,10 @@ def descargar_archivo_youtube(url, itag):
             ydl.download([url])
 
         if not os.path.isfile(ruta):
-            print("Archivo no generado:", ruta)
-            return jsonify({"error": "El archivo no fue generado correctamente."}), 500
+            return jsonify({"error": "El archivo no fue generado correctamente"}), 500
 
-        print("Archivo generado correctamente:", ruta)
         response = send_file(ruta, as_attachment=True)
-        os.remove(ruta)  # Borrar después de enviar
+        os.remove(ruta)  # Eliminar después de enviar
         return response
 
     except Exception:
